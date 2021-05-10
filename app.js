72d6f496b1ddc3a2142682345512f739f1e380ab
Vy Tran <vtran1022@Gmail.com>
2021-05-10 11:24:56 -0700

    Implementing code changes - added user event Click

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-var GroceryListItem = (props) => (
+var GroceryListItem = (props) => {
+  var onListItemClick = (event) => {
+    console.log('I got clicked');
+  }
+
+  return (
   <ul>
-    <li>{props.groceryItems[0]}</li>
+    <li onClick={onListItemClick}>{props.groceryItems[0]}</li>
     <li>{props.groceryItems[1]}</li>
+    <li>{props.groceryItems[2]}</li>
   </ul>
-);
+  );
+}
 
 var GroceryList = () => (
   <div>
     <h2>My Grocery List</h2>
-    <GroceryListItem groceryItems={['Milk', 'Eggs']}/>
+    <GroceryListItem groceryItems={['milk', 'eggs', 'bread']}/>
   </div>
 );
 
