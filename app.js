9bded8a9c186444803a8b894c73dfd9036c1eb76
Vy Tran <vtran1022@Gmail.com>
2021-05-10 12:14:31 -0700

    Implementing code - successfully added hovering makes text bold to code.. no actual hover event in react, use onMouseOver

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 class GroceryListItem extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      hover: false
+    };
+  }
+
+  onListItemHover() {
+    this.setState({
+      hover: !this.state.hover
+    });
   }
 
   render() {
+    var style = {
+      fontWeight: this.state.hover ? 'bold' : 'normal'
+    }
+
     return (
-      <li>{this.props.groceryItem}</li>
+      <li style={style} onMouseOver={this.onListItemHover.bind(this)}>{this.props.groceryItem}</li>
     );
   }
 }
